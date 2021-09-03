f088aed32eff18cefdb429bd7eaa47de6696e732
OLGA <olichka23.85@gmail.com>
2021-09-03 12:57:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+function writeCards() {
+    return [ 'Thank you, Lisa, for the wonderful surprise gift!', 'Thank you, Kaitlin, for the wonderful surprise gift!', 'Thank you, Jan, for the wonderful surprise gift!' ]
+}
+
+function countDown( num ) {
+    for (let i = num; i >= 0; i--)
+        console.log(i);
+}
